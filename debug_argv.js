d0eca429251d67c8b5c6c8b75bbd34950bde3cb6
loveseweater <69399784+loveseweater@users.noreply.github.com>
2026-06-11 19:05:35 -0700

    chore: cleanup debug files

--- debug_argv.js
+++
@@ -1,3 +0,0 @@
-
-console.log("process.argv:", JSON.stringify(process.argv));
-console.log("process.execArgv:", JSON.stringify(process.execArgv));
--- run_cf_build.js
+++
@@ -1,6 +0,0 @@
-
-// Fix process.argv - commander uses this
-process.argv = [process.argv[0], process.argv[1]];
-
-// Now require and run
-require("./node_modules/@cloudflare/next-on-pages/dist/index.js");
